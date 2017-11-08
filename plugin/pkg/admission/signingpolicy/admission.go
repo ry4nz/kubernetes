f5be9d3e104ad42d7a920d36226f4c31ef390736
Josh Hawn <jlhawn@berkeley.edu>
2017-11-08 11:36:09 -0800

    Use imagePullSecrets with DCT admission controller (#4)
    This patch updates the DCT admission controller to supply image pull
    secrets to the UCP webhook when resolving an image to a reference that
    meets the configured DCT policy. It also changes the expected behavior
    of the endpoint to be a batch endpoint to which it can supply a list
    of images and expect a mapping of the images in return. Any images which
    are not in the returned mapping are understood to NOT meet the current
    signing policy.
    
    Docker-DCO-1.1-Signed-off-by: Josh Hawn <josh.hawn@docker.com> (github: jlhawn)

--- plugin/pkg/admission/signingpolicy/admission.go
+++ plugin/pkg/admission/signingpolicy/admission.go
@@ -2,7 +2,8 @@ package signingpolicy
 
 import (
 	"crypto/tls"
-	"errors"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -10,12 +11,20 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/docker/go-connections/tlsconfig"
 	log "github.com/sirupsen/logrus"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/admission"
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/apps"
+	"k8s.io/kubernetes/pkg/apis/batch"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	kubeapiserveradmission "k8s.io/kubernetes/pkg/kubeapiserver/admission"
 )
 
 const (
@@ -25,27 +34,39 @@ const (
 	apiPath = "/api/dct/resolveimage"
 )
 
-const (
-	PluginName = "CheckImageSigning"
-)
+type dctResolveImageResponse struct {
+	ResolvedImages map[string]string `json:"resolvedImages"`
+	ErrorMessages  []string          `json:"errorMessages"`
+}
 
 // Register registers a plugin
 func Register(plugins *admission.Plugins) {
 	plugins.Register("CheckImageSigning", func(config io.Reader) (admission.Interface, error) {
 		var tlsConfig *tls.Config
 		var err error
-		certDir := os.Getenv("CERT_DIR")
-		if certDir != "" {
-			tlsOptions := tlsconfig.Options{
-				CAFile:   filepath.Join(certDir, rootCA),
-				CertFile: filepath.Join(certDir, cert),
-				KeyFile:  filepath.Join(certDir, key),
-			}
-			tlsConfig, err = tlsconfig.Client(tlsOptions)
-			if err != nil {
-				return nil, err
-			}
+
+		ucpURL := os.Getenv("UCP_URL")
+		if ucpURL == "" {
+			return nil, fmt.Errorf("UCP controller location not configured")
 		}
+		if !strings.HasPrefix(strings.ToLower(ucpURL), "https://") {
+			return nil, fmt.Errorf("UCP conroller location must use HTTPS")
+		}
+
+		certDir := os.Getenv("CERT_DIR")
+		if certDir == "" {
+			return nil, fmt.Errorf("certificate directory not specified: image signing policy webhook MUST use mutual TLS authentication")
+		}
+
+		tlsConfig, err = tlsconfig.Client(tlsconfig.Options{
+			CAFile:   filepath.Join(certDir, rootCA),
+			CertFile: filepath.Join(certDir, cert),
+			KeyFile:  filepath.Join(certDir, key),
+		})
+		if err != nil {
+			return nil, fmt.Errorf("unable to configure TLS client: %s", err)
+		}
+
 		httpClient := &http.Client{
 			Transport: &http.Transport{
 				TLSClientConfig: tlsConfig,
@@ -56,100 +77,186 @@ func Register(plugins *admission.Plugins) {
 			},
 		}
 
-		return NewSignedImage(httpClient), nil
+		return NewSignedImage(httpClient, ucpURL), nil
 	})
 }
 
 type signingPolicy struct {
 	*admission.Handler
-	ucpLocation string
-	httpClient  *http.Client
+	ucpWebHookURL      string
+	httpClient         *http.Client
+	internalKubeClient internalclientset.Interface
+	systemUserPrefix   string
 }
 
-func (a *signingPolicy) Admit(attributes admission.Attributes) (err error) {
-	// Ignore all calls to subresources or resources other than pods.
-	if len(attributes.GetSubresource()) != 0 || attributes.GetResource().GroupResource() != api.Resource("pods") {
-		return nil
+//NewSignedImage returns a signing policy handler
+func NewSignedImage(httpClient *http.Client, ucpURL string) admission.Interface {
+	return &signingPolicy{
+		Handler:          admission.NewHandler(admission.Create, admission.Update),
+		ucpWebHookURL:    ucpURL + apiPath,
+		httpClient:       httpClient,
+		systemUserPrefix: os.Getenv("SYSTEM_PREFIX"),
 	}
-	user := attributes.GetUserInfo().GetName()
-	log.Debugf("the user is: %s", user)
-	pod, ok := attributes.GetObject().(*api.Pod)
-	if !ok {
-		return apierrors.NewBadRequest("Resource was marked with kind Pod but was unable to be converted")
-	}
+}
 
-	for i, container := range pod.Spec.InitContainers {
-		image := container.Image
-		resolved, err := a.resolve(image, user)
-		if err != nil {
-			log.Infof("Reject image %s", image)
-			return err
-		}
-		log.Infof("Accept image %s, using now %s", image, resolved)
-		pod.Spec.InitContainers[i].Image = resolved
-	}
+var _ = kubeapiserveradmission.WantsInternalKubeClientSet(&signingPolicy{})
 
-	for i, container := range pod.Spec.Containers {
-		image := container.Image
-		resolved, err := a.resolve(image, user)
-		if err != nil {
-			log.Infof("Reject image %s", image)
-			return err
-		}
-		log.Infof("Accept image %s, using now %s", image, resolved)
-		pod.Spec.Containers[i].Image = resolved
+func (a *signingPolicy) SetInternalKubeClientSet(internalKubeClient internalclientset.Interface) {
+	a.internalKubeClient = internalKubeClient
+}
+
+func (a *signingPolicy) Validate() error {
+	if a.internalKubeClient == nil {
+		return fmt.Errorf("missing internal kube client")
 	}
 	return nil
 }
 
-//NewSignedImage returns a signing policy handler
-func NewSignedImage(httpClient *http.Client) admission.Interface {
-	return &signingPolicy{
-		Handler:     admission.NewHandler(admission.Create, admission.Update),
-		ucpLocation: os.Getenv("UCP_URL"),
-		httpClient:  httpClient,
+func getPodSpecFromObject(runtimeObject runtime.Object) *api.PodSpec {
+	switch object := runtimeObject.(type) {
+	case *api.Pod:
+		return &object.Spec
+	case *api.PodTemplate:
+		return &object.Template.Spec
+	case *api.ReplicationController:
+		return &object.Spec.Template.Spec
+	case *apps.StatefulSet:
+		return &object.Spec.Template.Spec
+	case *extensions.DaemonSet:
+		return &object.Spec.Template.Spec
+	case *extensions.Deployment:
+		return &object.Spec.Template.Spec
+	case *extensions.ReplicaSet:
+		return &object.Spec.Template.Spec
+	case *batch.Job:
+		return &object.Spec.Template.Spec
+	case *batch.CronJob:
+		return &object.Spec.JobTemplate.Spec.Template.Spec
+	default:
+		// Ignore all calls for objects that do not contain a Pod Spec.
+		return nil
 	}
 }
 
-func (a *signingPolicy) resolve(image, user string) (string, error) {
-	if a.ucpLocation == "" {
-		return "", errors.New("unable to resolve image: UCP controller location not configured")
+func (a *signingPolicy) Admit(attributes admission.Attributes) (err error) {
+	defer func() {
+		if err != nil {
+			log.Errorf("[DCT Signing Policy]: %s", err)
+		}
+	}()
+
+	if a.systemUserPrefix != "" && strings.HasPrefix(attributes.GetUserInfo().GetName(), a.systemUserPrefix) {
+		// Give system components a free pass. These are usually requests from
+		// a system component (like the kube controller manager) which is
+		// creating or updating a sub-object for which there would be a PodSpec
+		// which has already gone through admission control (e.g., when a
+		// Deployment was created).
+		return nil
 	}
-	img, err := a.checkUCPSigningPolicy(image, user)
+
+	podSpec := getPodSpecFromObject(attributes.GetObject())
+	if podSpec == nil {
+		return nil // The object does not contain a Pod Spec.
+	}
+
+	// Images from private registries or repositories require the user to
+	// specify image pull secrets so that the kubelet can have access to pull
+	// the images. The same credentials are also necessary when looking up a
+	// Notary repository.
+	imagePullSecretsData := make([]string, len(podSpec.ImagePullSecrets))
+	for i, secretRef := range podSpec.ImagePullSecrets {
+		secret, err := a.internalKubeClient.Core().Secrets(attributes.GetNamespace()).Get(secretRef.Name, metav1.GetOptions{})
+		if err != nil {
+			return admission.NewForbidden(attributes, fmt.Errorf("unable to lookup image pull secret %q from namespace %q: %s", secretRef.Name, attributes.GetNamespace(), err))
+		}
+
+		// The data is a JSON object. Something like:
+		//   {"registry.example.com":{"authCreds":"secretcredentialvalue"}}
+		if cfgData, ok := secret.Data[".dockercfg"]; ok {
+			imagePullSecretsData[i] = base64.URLEncoding.EncodeToString(cfgData)
+		}
+	}
+
+	// Put the container image names into a set to de-duplicate the values.
+	containerImageSet := map[string]struct{}{}
+	for _, container := range podSpec.InitContainers {
+		containerImageSet[container.Image] = struct{}{}
+	}
+	for _, container := range podSpec.Containers {
+		containerImageSet[container.Image] = struct{}{}
+	}
+
+	// Convert the set back into a list.
+	containerImages := make([]string, 0, len(containerImageSet))
+	for containerImage := range containerImageSet {
+		containerImages = append(containerImages, containerImage)
+	}
+
+	response, err := a.resolveImages(containerImages, imagePullSecretsData)
 	if err != nil {
-		return "", err
+		return apierrors.NewInternalError(err)
 	}
-	if img == "" {
-		return "", fmt.Errorf("%s is not signed", image)
+
+	log.Infof("[DCT Signing Policy] Resolved Images: %s", response.ResolvedImages)
+	log.Infof("[DCT Signing Policy] Error Messages: %s", response.ErrorMessages)
+
+	// Update the pod spec with the resolved images. If an image in not in
+	// the resolved mapping then it does not meet the signing requirements.
+	policyViolatingImageSet := map[string]struct{}{}
+
+	for i, container := range podSpec.InitContainers {
+		if resolvedImage, ok := response.ResolvedImages[container.Image]; ok {
+			podSpec.InitContainers[i].Image = resolvedImage
+		} else {
+			policyViolatingImageSet[container.Image] = struct{}{}
+		}
 	}
-	return img, nil
+	for i, container := range podSpec.Containers {
+		if resolvedImage, ok := response.ResolvedImages[container.Image]; ok {
+			podSpec.Containers[i].Image = resolvedImage
+		} else {
+			policyViolatingImageSet[container.Image] = struct{}{}
+		}
+	}
+
+	// If there were any images which violated the policy, return an error
+	// which also notifies about the bad images.
+	if len(policyViolatingImageSet) > 0 || len(response.ErrorMessages) > 0 {
+		policyViolatingImages := make([]string, 0, len(policyViolatingImageSet))
+		for image := range policyViolatingImageSet {
+			policyViolatingImages = append(policyViolatingImages, image)
+		}
+		return admission.NewForbidden(attributes, fmt.Errorf("one or more images do not meet the required signing policy: %s; additional error messages: %s", policyViolatingImages, response.ErrorMessages))
+	}
+
+	return nil
 }
 
-func (a *signingPolicy) checkUCPSigningPolicy(image, user string) (string, error) {
+func (a *signingPolicy) resolveImages(containerImages, imagePullSecrets []string) (*dctResolveImageResponse, error) {
+	apiArgs := url.Values{
+		"containerImage":  containerImages,
+		"imagePullSecret": imagePullSecrets,
+	}
 
-	apiArgs := url.Values{}
-	apiArgs.Set("image", image)
-	apiArgs.Set("user", user)
-	resp, err := a.httpClient.PostForm(a.ucpLocation+apiPath, apiArgs)
+	resp, err := a.httpClient.PostForm(a.ucpWebHookURL, apiArgs)
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("unable to perform UCP DCT image resolve request: %s", err)
 	}
 	defer resp.Body.Close()
+
 	//check status code
 	if resp.StatusCode != http.StatusOK {
-		if resp.StatusCode == http.StatusForbidden {
-			return "", nil
+		err = fmt.Errorf("UCP DCT image resolve request returned status %d", resp.StatusCode)
+		if bodyContent, err := ioutil.ReadAll(resp.Body); err == nil {
+			err = fmt.Errorf("%s: %s", err, bodyContent)
+		}
+		return nil, err
+	}
 
-		}
-		errMsg, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return "", fmt.Errorf("UCP returned http statuscode %s", resp.StatusCode)
-		}
-		return "", fmt.Errorf("UCP returned http statuscode %d: %s", resp.StatusCode, errMsg)
+	var response dctResolveImageResponse
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return nil, fmt.Errorf("could not parse UCP DCT image resolve response as JSON: %s", err)
 	}
-	img, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(img), nil
+
+	return &response, nil
 }
